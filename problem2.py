6ab82ce95259a30b3dedc8bb5aa450222909af25
DanDoesMusic <danielgirgis779@gmail.com>
2021-02-23 15:31:24 +0000

    I made a minor mistake and just now noticed

--- problem2.py
+++ problem2.py
@@ -6,11 +6,11 @@ opp = float(input("what is the lenght of the opposite side: "))
 hyp = float(input("what is the lenght of the hypotenouse: "))
 ###this takes the 3 mesurments down###
 
-if (adj + opp) == hyp : 
+if (adj + opp) >= hyp : 
   print("this is a triangle")
-elif (hyp + opp) == adj : 
+elif (hyp + opp) >= adj : 
   print("this is a triangle")
-elif (adj + hyp) == opp : 
+elif (adj + hyp) >= opp : 
   print("this is a triangle")
 else :
   print("this is not a triangle")
