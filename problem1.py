77806a223c50819669eff167821798342b771633
DanDoesMusic <danielgirgis779@gmail.com>
2021-02-23 15:15:27 +0000

    I did the work Hail Mary, Full of Grace, The Lord is with thee. Blessed art thou among women, and blessed is the fruit of thy womb, Jesus. Holy Mary, Mother of God, pray for us sinners now, and at the hour of death. Glory Be to the Father, and to the Son, and to the Holy Spirit. amen

--- problem1.py
+++ problem1.py
@@ -1,1 +1,19 @@
+###this takes the 2 speeds the limit and the users speed###
 
+speed = int(input("how fast was the diver: "))
+limit = int(input("what is the speed limit: "))
+###this chacks if they are above the speed limit it took me a bit but i got it working###
+
+if (speed - limit) >= 1 :
+  print("your fine is 100$")
+
+elif (speed - limit) >= 21 :
+  print("your fine is 270$")
+
+elif (speed - limit) >= 31 :
+  print("your fine is 570$")
+
+else :
+  print ("you are within the limit")
+
+###it checks to see if you are above 1 u/h or unit per hour then it checks if you are 21 u/h above then 31 or more and determines your fine based on what you cross if you are below or at the limit you are safe###
--- problem2.py
+++ problem2.py
@@ -1,1 +1,22 @@
+###so it takes the 3 side mesures and it adds all the combinations together if any 1 of them adds up to 2 of the others it is a triangle###
 
+print("welcome to the triangle checker")
+adj = float(input("what is the lenght of the adjacent side: "))
+opp = float(input("what is the lenght of the opposite side: "))
+hyp = float(input("what is the lenght of the hypotenouse: "))
+###this takes the 3 mesurments down###
+
+if (adj + opp) == hyp : 
+  print("this is a triangle")
+elif (hyp + opp) == adj : 
+  print("this is a triangle")
+elif (adj + hyp) == opp : 
+  print("this is a triangle")
+else :
+  print("this is not a triangle")
+print("have a nice day")
+###this is the maths part of my code###
+
+
+
+###it just works###
